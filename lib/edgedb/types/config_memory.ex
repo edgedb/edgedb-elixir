3a41b8b05e6989af6e13f97513c94dd903c31048
Nik <sidnev.nick@gmail.com>
2022-02-02 19:25:28 +0300

    add custom inspect for 'cal::relative_duration' and 'cfg::memory' (#40)

--- lib/edgedb/types/config_memory.ex
+++ lib/edgedb/types/config_memory.ex
@@ -6,4 +6,45 @@ defmodule EdgeDB.ConfigMemory do
   @opaque t() :: %__MODULE__{
             bytes: pos_integer()
           }
+
+  @spec bytes(t()) :: pos_integer()
+  def bytes(%__MODULE__{bytes: bytes}) do
+    bytes
+  end
+end
+
+defimpl Inspect, for: EdgeDB.ConfigMemory do
+  import Inspect.Algebra
+
+  @kib 1024
+  @mib 1024 * @kib
+  @gib 1024 * @mib
+  @tib 1024 * @gib
+  @pib 1024 * @tib
+
+  @impl Inspect
+  def inspect(%EdgeDB.ConfigMemory{bytes: bytes}, _opts) do
+    bytes_repr =
+      cond do
+        bytes >= @pib and rem(bytes, @pib) == 0 ->
+          "#{div(bytes, @pib)}PiB"
+
+        bytes >= @tib and rem(bytes, @tib) == 0 ->
+          "#{div(bytes, @tib)}TiB"
+
+        bytes >= @gib and rem(bytes, @gib) == 0 ->
+          "#{div(bytes, @gib)}GiB"
+
+        bytes >= @mib and rem(bytes, @mib) == 0 ->
+          "#{div(bytes, @mib)}MiB"
+
+        bytes >= @kib and rem(bytes, @kib) == 0 ->
+          "#{div(bytes, @kib)}KiB"
+
+        true ->
+          "#{bytes}B"
+      end
+
+    concat(["#EdgeDB.ConfigMemory<\"", bytes_repr, "\">"])
+  end
 end
--- lib/edgedb/types/relative_duration.ex
+++ lib/edgedb/types/relative_duration.ex
@@ -9,3 +9,89 @@ defmodule EdgeDB.RelativeDuration do
           microseconds: pos_integer()
         }
 end
+
+defimpl Inspect, for: EdgeDB.RelativeDuration do
+  import Inspect.Algebra
+
+  @months_per_year 12
+  @usecs_per_hour 3_600_000_000
+  @usecs_per_minute 60_000_000
+  @usecs_per_sec 1_000_000
+
+  @impl Inspect
+  def inspect(%EdgeDB.RelativeDuration{months: 0, days: 0, microseconds: 0}, _opts) do
+    concat(["#EdgeDB.RelativeDuration<\"", "PT0S", "\">"])
+  end
+
+  @impl Inspect
+  def inspect(%EdgeDB.RelativeDuration{} = duration, _opts) do
+    duration_repr =
+      "P"
+      |> format_date(duration)
+      |> format_time(duration)
+
+    concat(["#EdgeDB.RelativeDuration<\"", duration_repr, "\">"])
+  end
+
+  defp format_date(formatted_repr, %EdgeDB.RelativeDuration{} = duration) do
+    formatted_repr
+    |> maybe_add_time_part(div(duration.months, @months_per_year), "Y")
+    |> maybe_add_time_part(rem(duration.months, @months_per_year), "M")
+    |> maybe_add_time_part(duration.days, "D")
+  end
+
+  defp format_time(formatted_repr, %EdgeDB.RelativeDuration{microseconds: 0}) do
+    formatted_repr
+  end
+
+  defp format_time(formatted_repr, %EdgeDB.RelativeDuration{microseconds: time}) do
+    formatted_repr = "#{formatted_repr}T"
+
+    tfrac = div(time, @usecs_per_hour)
+    time = time - tfrac * @usecs_per_hour
+    hour = tfrac
+
+    tfrac = div(time, @usecs_per_minute)
+    time = time - tfrac * @usecs_per_minute
+    min = tfrac
+
+    formatted_repr =
+      formatted_repr
+      |> maybe_add_time_part(hour, "H")
+      |> maybe_add_time_part(min, "M")
+
+    sec = div(time, @usecs_per_sec)
+    fsec = time - sec * @usecs_per_sec
+
+    sign =
+      if min < 0 or fsec < 0 do
+        "-"
+      else
+        ""
+      end
+
+    if sec != 0 or fsec != 0 do
+      formatted_repr = "#{formatted_repr}#{sign}#{abs(sec)}"
+
+      formatted_repr =
+        if fsec != 0 do
+          fsec = String.trim_trailing("#{abs(fsec)}", "0")
+          "#{formatted_repr}.#{fsec}"
+        else
+          formatted_repr
+        end
+
+      "#{formatted_repr}S"
+    else
+      formatted_repr
+    end
+  end
+
+  defp maybe_add_time_part(formatted_repr, 0, _letter) do
+    formatted_repr
+  end
+
+  defp maybe_add_time_part(formatted_repr, value, letter) do
+    "#{formatted_repr}#{value}#{letter}"
+  end
+end
