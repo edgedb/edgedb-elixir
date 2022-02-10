45c47d629eb38fcf8191b82ad6dab4a215566966
Nik <sidnev.nick@gmail.com>
2022-02-10 19:23:13 +0300

    add metadata for package (#48)

--- mix.exs
+++ mix.exs
@@ -1,36 +1,29 @@
 defmodule EdgeDB.MixProject do
   use Mix.Project
 
-  @version "0.0.0"
+  @app :edgedb
+  @version "0.1.0"
+  @source_url "https://github.com/nsidnev/edgedb-elixir"
+  @description "EdgeDB driver for Elixir"
 
   def project do
     [
-      app: :edgedb,
+      app: @app,
       version: @version,
-      elixir: "~> 1.10",
+      elixir: "~> 1.12",
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
-      elixirc_options: [
-        warnings_as_errors: true
-      ],
+      elixirc_options: elixirc_options(),
+      start_permanent: Mix.env() == :prod,
       consolidate_protocols: Mix.env() != :test,
-      test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [
-        dialyzer: :test,
-        credo: :test,
-        coveralls: :test,
-        "coveralls.detail": :test,
-        "coveralls.github": :test,
-        "coveralls.html": :test
-      ],
-      dialyzer: [
-        plt_add_apps: [:ex_unit],
-        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
-      ],
+      test_coverage: test_coverage(),
+      preferred_cli_env: preferred_cli_env(),
+      dialyzer: dialyzer(),
       aliases: aliases(),
       name: "EdgeDB",
-      description: "EdgeDB driver for Elixir",
-      docs: docs()
+      description: @description,
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -53,17 +46,65 @@ defmodule EdgeDB.MixProject do
       {:decimal, "~> 2.0"},
       {:jason, "~> 1.2", optional: true},
       # dev/test
-      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.2", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.14", only: :test},
-      {:mox, "~> 1.0", only: :test}
+      {:mox, "~> 1.0", only: :test},
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false}
+    ]
+  end
+
+  defp elixirc_paths(:test) do
+    ["lib", "test/support"]
+  end
+
+  defp elixirc_paths(_env) do
+    ["lib"]
+  end
+
+  defp elixirc_options do
+    [
+      warnings_as_errors: true
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_add_apps: [:ex_unit],
+      plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+    ]
+  end
+
+  defp test_coverage do
+    [
+      tool: ExCoveralls
+    ]
+  end
+
+  defp preferred_cli_env do
+    [
+      dialyzer: :test,
+      credo: :test,
+      coveralls: :test,
+      "coveralls.detail": :test,
+      "coveralls.github": :test,
+      "coveralls.html": :test
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Nik Sidnev"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @source_url
+      }
     ]
   end
 
   defp docs do
     [
-      source_url: "https://github.com/nsidnev/edgedb-elixir",
+      source_url: @source_url,
       source_ref: "v#{@version}",
       main: "main",
       extras: [
@@ -89,14 +130,6 @@ defmodule EdgeDB.MixProject do
         ]
       ]
     ]
-  end
-
-  defp elixirc_paths(:test) do
-    ["lib", "test/support"]
-  end
-
-  defp elixirc_paths(_env) do
-    ["lib"]
   end
 
   defp aliases do
