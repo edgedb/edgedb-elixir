2f07bd663750106c01b8567395c046c7a5194fdb
Nik Sidnev <sidnev.nick@gmail.com>
2022-08-01 22:06:57 +0300

    allow to skip test dynamically

--- test/edgedb/protocol/codecs/range_test.exs
+++ test/edgedb/protocol/codecs/range_test.exs
@@ -17,64 +17,81 @@ defmodule Tests.EdgeDB.Protocol.Codecs.RangeTest do
   setup :edgedb_connection
 
   setup %{conn: conn} do
-    range_type = EdgeDB.query!(conn, "select schema::ObjectType filter .name = 'schema::Range'")
+    range_type =
+      EdgeDB.query_single!(
+        conn,
+        "select schema::ObjectType filter .name = 'schema::Range' limit 1"
+      )
 
-    if EdgeDB.Set.empty?(range_type) do
+    if range_type do
       Logger.warn("skip #{__MODULE__} since server has not support for std::ranges")
-      %{skip: true}
+      %{edgedb_v1: true}
     else
-      :ok
+      %{edgedb_v1: true}
     end
   end
 
-  test "decoding range value with lower bound and without upper bound", %{conn: conn} do
-    value = EdgeDB.Range.new(1, 10)
-    assert ^value = EdgeDB.query_required_single!(conn, "select range(1, 10)")
+  test "decoding range value with lower bound and without upper bound", %{conn: conn} = ctx do
+    skip_if :edgedb_v1, "missing ranges support" do
+      value = EdgeDB.Range.new(1, 10)
+      assert ^value = EdgeDB.query_required_single!(conn, "select range(1, 10)")
+    end
   end
 
-  test "decoding range value without lower bound and without upper bound", %{conn: conn} do
-    value = EdgeDB.Range.new(2, 10)
+  test "decoding range value without lower bound and without upper bound", %{conn: conn} = ctx do
+    skip_if :edgedb_v1, "missing ranges support" do
+      value = EdgeDB.Range.new(2, 10)
 
-    assert ^value =
-             EdgeDB.query_required_single!(
-               conn,
-               "select range(1, 10, inc_lower := false)"
-             )
+      assert ^value =
+               EdgeDB.query_required_single!(
+                 conn,
+                 "select range(1, 10, inc_lower := false)"
+               )
+    end
   end
 
-  test "decoding range value with lower bound and with upper bound", %{conn: conn} do
-    value = EdgeDB.Range.new(1, 11)
+  test "decoding range value with lower bound and with upper bound", %{conn: conn} = ctx do
+    skip_if :edgedb_v1, "missing ranges support" do
+      value = EdgeDB.Range.new(1, 11)
 
-    assert ^value =
-             EdgeDB.query_required_single!(
-               conn,
-               "select range(1, 10, inc_upper := true)"
-             )
+      assert ^value =
+               EdgeDB.query_required_single!(
+                 conn,
+                 "select range(1, 10, inc_upper := true)"
+               )
+    end
   end
 
-  test "decoding range value without lower bound and with upper bound", %{conn: conn} do
-    value = EdgeDB.Range.new(2, 11)
+  test "decoding range value without lower bound and with upper bound", %{conn: conn} = ctx do
+    skip_if :edgedb_v1, "missing ranges support" do
+      value = EdgeDB.Range.new(2, 11)
 
-    assert ^value =
-             EdgeDB.query_required_single!(
-               conn,
-               "select range(1, 10, inc_lower := false, inc_upper := true)"
-             )
+      assert ^value =
+               EdgeDB.query_required_single!(
+                 conn,
+                 "select range(1, 10, inc_lower := false, inc_upper := true)"
+               )
+    end
   end
 
-  test "decoding float range value without lower bound and with upper bound", %{conn: conn} do
-    value = EdgeDB.Range.new(1.1, 3.3, inc_lower: false, inc_upper: true)
+  test "decoding float range value without lower bound and with upper bound",
+       %{conn: conn} = ctx do
+    skip_if :edgedb_v1, "missing ranges support" do
+      value = EdgeDB.Range.new(1.1, 3.3, inc_lower: false, inc_upper: true)
 
-    assert ^value =
-             EdgeDB.query_required_single!(
-               conn,
-               "select range(1.1, 3.3, inc_lower := false, inc_upper := true)"
-             )
+      assert ^value =
+               EdgeDB.query_required_single!(
+                 conn,
+                 "select range(1.1, 3.3, inc_lower := false, inc_upper := true)"
+               )
+    end
   end
 
-  test "decoding range value with empty range", %{conn: conn} do
-    value = EdgeDB.Range.empty()
-    assert ^value = EdgeDB.query_required_single!(conn, "select range(1, 1)")
+  test "decoding range value with empty range", %{conn: conn} = ctx do
+    skip_if :edgedb_v1, "missing ranges support" do
+      value = EdgeDB.Range.empty()
+      assert ^value = EdgeDB.query_required_single!(conn, "select range(1, 1)")
+    end
   end
 
   for {type, values} <- @input_ranges do
@@ -89,9 +106,11 @@ defmodule Tests.EdgeDB.Protocol.Codecs.RangeTest do
         end
 
       test "encoding #{inspect(input)} as #{inspect(type)} with expecting #{inspect(output)} in the end",
-           %{conn: conn} do
-        value = "value"
-        assert ^value = EdgeDB.query_single!(conn, "select <short_str>$0", [value])
+           %{conn: conn} = ctx do
+        skip_if :edgedb_v1, "missing ranges support" do
+          value = "value"
+          assert ^value = EdgeDB.query_single!(conn, "select <short_str>$0", [value])
+        end
       end
     end
   end
