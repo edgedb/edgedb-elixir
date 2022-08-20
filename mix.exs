79b9a0392f196a7823b8aa28cad974385a45dfb3
Nik Sidnev <sidnev.nick@gmail.com>
2022-08-20 21:42:53 +0300

    increment package version in `mix.exs`

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule EdgeDB.MixProject do
   use Mix.Project
 
   @app :edgedb
-  @version "0.4.0"
+  @version "0.5.0"
   @source_url "https://github.com/nsidnev/edgedb-elixir"
   @description "EdgeDB client for Elixir"
 
