a88e66d44b38b269fe59caa2e97e88690f5bcf40
Nik <nik@edgedb.com>
2023-11-01 11:07:04 -0400

    fix CI by updating EdgeDB action (#179)

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule EdgeDB.MixProject do
   use Mix.Project
 
   @app :edgedb
-  @version "0.6.1"
+  @version "0.7.0"
   @source_url "https://github.com/edgedb/edgedb-elixir"
   @description "EdgeDB client for Elixir"
 
